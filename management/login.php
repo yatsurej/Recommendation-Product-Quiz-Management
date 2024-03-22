816e45d0cf3270f13365256279ce7c48fe551fb1
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-03-22 15:48:33 +0800

    Update login.php

--- management/login.php
+++ management/login.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
     <?php
         session_start();
 
@@ -164,3 +165,63 @@
         }
 
     </style>
+=======
+<?php
+session_start();
+
+if (isset($_SESSION['user_authenticated'])) {
+    header('Location: index.php');
+    exit();
+}
+
+$pageTitle = "Login Page";
+require '../db.php';
+include './header.php';
+?>
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+
+<div class="bg-cover min-h-screen flex flex-col items-center justify-center" style="background-image: url('../assets/images/background.png');">
+    <div class="absolute top-[1%] right-[1%]">
+        <img src="../assets/images/ada.png" alt="ADALogo" class="w-[70px] md:w-[75px] lg:w-[80px]">
+    </div>
+
+    <div class="text-center">
+        <h1 class="text-white text-3xl md:text-4xl lg:text-5xl mb-8">Quiz Dashboard</h1>
+    </div>
+
+    <div class="bg-white bg-opacity-10 rounded-lg p-8 w-11/12 md:w-1/2 lg:w-1/3">
+        <form action="functions.php" method="post" autocomplete="off">
+            <div class="mb-6">
+                <input type="text" placeholder="Your Mail" name="username" id="username" class="w-full h-12 p-3 rounded border-none" required>
+            </div>
+
+            <div class="relative mb-6">
+                <input type="password" id="password" placeholder="Your Password" name="password" class="w-full h-12 p-3 rounded border-none" required>
+                <button type="button" id="togglePassword" class="absolute top-1/2 transform -translate-y-1/2 right-4 bg-transparent border-none cursor-pointer">
+                    <i class="fas fa-eye" id="eye-icon"></i>
+                </button>
+            </div>
+
+            <button type="submit" class="w-full h-12 bg-adminblue text-white rounded border-none">Login</button>
+        </form>
+    </div>
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const passwordInput = document.getElementById('password');
+        const togglePasswordButton = document.getElementById('togglePassword');
+        const eyeIcon = document.getElementById('eye-icon');
+
+        togglePasswordButton.addEventListener('click', () => {
+            const type = passwordInput.type === 'password' ? 'text' : 'password';
+            passwordInput.type = type;
+
+            eyeIcon.classList.toggle('fa-eye-slash');
+        });
+    });
+</script>
+>>>>>>> Stashed changes
