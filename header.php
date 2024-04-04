eccfe14cc811d68ad31a220f346296a42712e493
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-04-04 10:23:23 +0800

    optimization

--- header.php
+++ header.php
@@ -6,12 +6,14 @@
     <link rel="icon" type="image/x-icon" href="./assets/fav-icon.png" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/css/multi-select-tag.css">
+
+
 </head>
 
 <?php
-    if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
-        header("Location: index.php");
-        exit();
-    }
-    include 'db.php';
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    header("Location: index.php");
+    exit();
+}
+include 'db.php';
 ?>
--- management/login.php
+++ management/login.php
@@ -1,55 +1,68 @@
 <?php
-    session_start();
+session_start();
 
-    if (isset($_SESSION['user_authenticated'])) {
-        header('Location: index.php');
-        exit();
-    }
+if (isset($_SESSION['user_authenticated'])) {
+    header('Location: index.php');
+    exit();
+}
 
-    $pageTitle = "Login Page";
-    require '../db.php';
-    include 'header.php';
-    include 'navbar.php';
+$pageTitle = "Login Page";
+require '../db.php';
+include './header.php';
 ?>
 
-<div class="container w-75">
-    <h1 class="text-start fw-bold mt-3">Log in</h1>
-    <hr style="height:1px;border-width:0;color:gray;background-color:gray">
-    <div class="row justify-content-center">
-        <form action="functions.php" method="post" autocomplete="off">
-            <div class="form-group">
-                <label class="fw-bold" for="username">Username</label>
-                <input class="form-control" type="text" name="username" id="username" placeholder="Enter username" required>
-            </div>
-            <div class="form-group">
-                <label class= "fw-bold" for="password">Password</label>
-                <div class="input-group">
-                    <input class="form-control" type="password" name="password" id="password" placeholder="Enter password" required>
-                    <div class="input-group-append">
-                        <span class="input-group-text" id="toggle-password">
-                            <i class="fa fa-eye" aria-hidden="true"></i>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <br>
-            <div class="text-center">
-                <button class="btn btn-dark mb-3" name="login">Log in</button>
-            </div>
-            <hr style="height:1px;border-width:0;color:gray;background-color:gray">
-        </form>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+
+<div class="login-page">
+
+    <div class="logocontainer">
+        <img src="../assets/images/PG.png" alt="ADALogo" class="ADALogo">
     </div>
+
+    <div class="login">
+
+        <div class="title">
+            <h1>Quiz Dashboard</h1>
+        </div>
+
+        <div class="login-form">
+            <form action="functions.php" method="post" autocomplete="off">
+                <div class="container login-container">
+
+                    <input type="text" placeholder="Username" name="username" id="username" required class="border-none">
+
+                    <div class="password-container">
+                        <input type="password" id="password" placeholder="Password" name="password" required>
+                        <button type="button" id="togglePassword">
+                            <i class="fas fa-eye" id="eye-icon"></i>
+                        </button>
+                    </div>
+
+
+                    <button type="submit" class="loginpage-button" name="login">Login</button>
+                </div>
+            </form>
+        </div>
+
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const passwordInput = document.getElementById('password');
+                const togglePasswordButton = document.getElementById('togglePassword'); // Update the id here
+                const eyeIcon = document.getElementById('eye-icon');
+
+                togglePasswordButton.addEventListener('click', () => {
+                    const type = passwordInput.type === 'password' ? 'text' : 'password';
+                    passwordInput.type = type;
+
+                    eyeIcon.classList.toggle('fa-eye-slash');
+                });
+            });
+        </script>
+
+
+    </div>
+
 </div>
-
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const passwordInput = document.getElementById('password');
-        const togglePasswordButton = document.getElementById('toggle-password');
-
-        togglePasswordButton.addEventListener('click', function () {
-            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-            passwordInput.setAttribute('type', type);
-            togglePasswordButton.innerHTML = type === 'password' ? '<i class="fa fa-eye" aria-hidden="true"></i>' : '<i class="fa fa-eye-slash" aria-hidden="true"></i>';
-        });
-    });
-</script>
