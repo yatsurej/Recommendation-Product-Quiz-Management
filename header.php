9841f971de057e46cf0c73b1c4861e4f30dc5c41
Froizel Rej Apolonio <froizelrej@gmail.com>
2024-04-16 14:35:29 +0800

    commit customization

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="./assets/styles.css">
+    <link rel="stylesheet" href="./assets/styles.php">
     <link rel="icon" type="image/x-icon" href="./assets/fav-icon.png" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/css/multi-select-tag.css">
